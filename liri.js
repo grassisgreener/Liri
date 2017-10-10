53922798470ebef9d32ae41fa7d0cb5a911d7386
grassisgreener <vany@uw.edu>
2017-10-10 16:39:29 -0500

    this

--- liri.js
+++ liri.js
@@ -4,10 +4,18 @@ var Twitter = require('twitter');
 
 var client = new Twitter(keys)
 
+var liriArgument = process.argv[1];
+
+switch (liriArgument) {
+    case "my-tweets":
+        params();
+        break;
+};
+
 var params = { screen_name: 'grassisgreen77' };
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 19; i++) {
             console.log(tweets[i].text);
         }
 
